98fddf0415f4acaea9c4f64f0664dc2c792a7cf6
garam Jo <latack789@naver.com>
2025-05-14 16:10:47 +0900

    feat: electron.ts를 js로 변환하기 위한 초기 세팅

---
+++ public/electron.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from "electron";
+import * as isDev from "electron-is-dev";
+import path from "node:path";
+// import * as path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
